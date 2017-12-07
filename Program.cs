ada0a3003289df47b2edd73826d662c8da4cfc4a
Renzo Schindeler <renzo@schindeler.nl>
2017-12-07 12:36:46 +0100

    Connection en Server classes uit template

--- Program.cs
+++ Program.cs
@@ -3,22 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
 
 namespace Concurrency_P2
 {
     class Program
     {
+        public static Dictionary<int, Connection> Buren = new Dictionary<int, Connection>();
+        public static ushort poortNummer;
+
         static void Main(string[] args)
         {
             // Input
             string firstLine = Console.ReadLine();
             string[] input = firstLine.Split();
-            Console.WriteLine(input[0]);
-            ushort poortNummer = ushort.Parse(input[0]);
-            ushort[] buurPoorten = new ushort[input.Length -1];
+            poortNummer = ushort.Parse(input[0]);
+            //ushort[] buurPoorten = new ushort[input.Length -1];
             for (int i = 1; i < input.Length; i++)
             {
-                buurPoorten[i - 1] = ushort.Parse(input[i]);
+                Buren.Add(int.Parse(input[i]), new Connection(int.Parse(input[i])));
             }
         }
     }
