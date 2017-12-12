42e0f14b19667e4b086eac4649cc30b54883c65e
Steven <stevenheinen01@gmail.com>
2017-12-12 17:26:41 +0100

    Betere versie

--- Program.cs
+++ Program.cs
@@ -3,73 +3,174 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 using System.Net.Sockets;
 using System.Net;
+using System.IO;
 using System.Threading;
 
-
-namespace Concurrency_P2
+namespace CCPract2
 {
     class Program
     {
-        public static Dictionary<int, Connection> Buren = new Dictionary<int, Connection>();
-        public static ushort poortNummer;
+        public static int myPort;
+        public static Dictionary<int, List<Connection>> neighbours = new Dictionary<int, List<Connection>>();
 
         static void Main(string[] args)
         {
-            // Input
-            string firstLine = Console.ReadLine();
-            string[] input = firstLine.Split();
-            poortNummer = ushort.Parse(input[0]);
+            myPort = int.Parse(args[0]);
 
-            Console.Title = "Netchange " + poortNummer;
+            Console.Title = "NetChange " + myPort;
 
-            new Server(poortNummer);
+            new Server(myPort);
 
-            //ushort[] buurPoorten = new ushort[input.Length -1];
-            for (int i = 1; i < input.Length; i++)
+            for (int i = 1; i < args.Length; i++)
             {
-                Buren.Add(int.Parse(input[i]), new Connection(int.Parse(input[i])));
-            }
-
-            while (true)
-            {
-                string[] commando = Console.ReadLine().Split();
-
-                if (commando[0] == "R")
-                {
-                    // Routing table
-                    RoutingTable();
-                }
-                else if (commando[0] == "B")
-                {
-                    int portNumber = int.Parse(commando[1]);
-                    string message = commando[2];
-                    // Send message to portnumber
-                }
-                else if (commando[0] == "C")
-                {
-                    int portNumber = int.Parse(commando[1]);
-                    // Connect to portnumber
-                }
-                else if (commando[0] == "D")
-                {
-                    int portNumber = int.Parse(commando[1]);
-                    // Disconnect from portnumber
-                }
+                int port = int.Parse(args[i]);
+                AddConnectionToDictionary(port, new Connection(port));
             }
         }
 
-        static void RoutingTable()
+        public static void AddConnectionToDictionary(int port, Connection connection)
         {
-            string result = "";
-            foreach (KeyValuePair<int, Connection> kv in Buren)
+            if (!neighbours.ContainsKey(port))
             {
-                int distance = 0;
-                result += kv.Key + " " + distance + " " + kv.Value + "\n";
+                List<Connection> list = new List<Connection>();
+                neighbours.Add(port, list);
             }
-            Console.WriteLine(result);
+            neighbours[port].Add(connection);
+        }
+    }
+
+    class Server
+    {
+        public Server(int port)
+        {
+            // Luister op de opgegeven poort naar verbindingen
+            TcpListener server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+
+            // Start een aparte thread op die verbindingen aanneemt
+            new Thread(() => AcceptLoop(server)).Start();
+        }
+
+        private void AcceptLoop(TcpListener handle)
+        {
+            while (true)
+            {
+                TcpClient client = handle.AcceptTcpClient();
+                StreamReader clientIn = new StreamReader(client.GetStream());
+                StreamWriter clientOut = new StreamWriter(client.GetStream());
+                clientOut.AutoFlush = true;
+
+                // De server weet niet wat de poort is van de client die verbinding maakt, de client geeft dus als onderdeel van het protocol als eerst een bericht met zijn poort
+                int hisPort = int.Parse(clientIn.ReadLine().Split()[1]);
+
+                Console.WriteLine("Verbonden: " + hisPort);
+
+                // Zet de nieuwe verbinding in de verbindingslijst
+                Program.AddConnectionToDictionary(hisPort, new Connection(clientIn, clientOut));
+            }
+        }
+    }
+
+    class Connection
+    {
+        public StreamReader Read;
+        public StreamWriter Write;
+
+        // Connection heeft 2 constructoren: deze constructor wordt gebruikt als wij CLIENT worden bij een andere SERVER
+        public Connection(int port)
+        {
+            here:
+            try
+            {
+                TcpClient client = new TcpClient("localhost", port);
+                Read = new StreamReader(client.GetStream());
+                Write = new StreamWriter(client.GetStream());
+                Write.AutoFlush = true;
+            }
+            catch
+            {
+                Thread.Sleep(5);
+                goto here;
+            }
+
+            // De server kan niet zien van welke poort wij client zijn, dit moeten we apart laten weten
+            Write.WriteLine("Poort: " + Program.myPort);
+
+            // Start het reader-loopje
+            new Thread(ReaderThread).Start();
+        }
+
+        // Deze constructor wordt gebruikt als wij SERVER zijn en een CLIENT maakt met ons verbinding
+        public Connection(StreamReader read, StreamWriter write)
+        {
+            Read = read; Write = write;
+
+            // Start het reader-loopje
+            new Thread(ReaderThread).Start();
+        }
+
+        // LET OP: Nadat er verbinding is gelegd, kun je vergeten wie er client/server is (en dat kun je aan het Connection-object dus ook niet zien!)
+
+        // Deze loop leest wat er binnenkomt en print dit
+        public void ReaderThread()
+        {
+            try
+            {
+                while (true)
+                {
+                    string[] input = Read.ReadLine().Split();
+                    //Console.WriteLine(input);
+
+
+                    Console.WriteLine("Console");
+                    Write.WriteLine("Write");
+
+                    // Gebruiker vraagt om routing table
+                    if (input[0] == "R")
+                    {
+                        // Print routing table
+
+                        int distance = 0;
+                        Write.WriteLine(Program.myPort + " " + distance + " local");
+                        distance++;
+                        foreach (KeyValuePair<int, List<Connection>> kv in Program.neighbours)
+                        {
+                            Write.WriteLine(kv.Key + " " + distance + " " + kv.Key);
+                        }
+                    }
+                    else if (input[0] == "B")
+                    {
+                        int port = int.Parse(input[1]);
+                        string message = "";
+                        for (int i = 2; i < input.Length - 1; i++)
+                        {
+                            message += input[2] + " ";
+                        }
+                        message += input[input.Length - 1];
+
+                        // Send Message to Port
+                    }
+                    else if (input[0] == "C")
+                    {
+                        int port = int.Parse(input[1]);
+
+                        // Connect to Port
+                    }
+                    else if (input[0] == "D")
+                    {
+                        int port = int.Parse(input[1]);
+
+                        // Disconnect from Port
+                    }
+                }
+            }
+            catch
+            {
+                // Verbinding is kennelijk verbroken
+                Console.WriteLine("Verbroken: " + Program.myPort);
+            }
         }
     }
 }
